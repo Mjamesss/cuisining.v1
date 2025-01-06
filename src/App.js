a0e3b476e23de42e066d4507326814d87d1359c6
Rafael <rafael.markjames.bscs2022@gmail.com>
2025-01-06 15:04:05 +0800

    f

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Button, Container, Row, Col } from 'react-bootstrap';
-import L from './components/L'; // Import Login Component
-import S from './components/S'; // Import Sign Up Component
-import ForgotPass from './components/ForgotPass'; // Import Forgot Password Component
-import OTP from './components/OTP'; // Import OTP Component
-import SetNewPass from './components/SetNewPass'; // Import Set New Password Component
-import Done from './components/Done'; // Import Done Component
-import Landing from './components/Landing'; // Import Home Component
-import DoneRegister from './components/DoneRegister'; // Import Done Register Component
+import L from './pages/L'; // Import Login Component
+import S from './pages/S'; // Import Sign Up Component
+import ForgotPass from './pages/ForgotPass'; // Import Forgot Password Component
+import OTP from './pages/OTP'; // Import OTP Component
+import SetNewPass from './pages/SetNewPass'; // Import Set New Password Component
+import Done from './pages/Done'; // Import Done Component
+import Landing from './pages/Landing'; // Import Home Component
+import DoneRegister from './pages/DoneRegister'; // Import Done Register Component
 
 const App = () => {
   return (
