61cb2870a47c6c290478851f24d1aa597f6a165b
karlbrizuela <karl.brizuela01@gmail.com>
2025-02-20 01:43:09 +0800

    adding measuring karl

--- src/pages/utensilsEng/Scissors.jsx
+++ src/pages/utensilsEng/Scissors.jsx
@@ -13,28 +13,25 @@ const Scissors = () =>{
         <div className="d-grid g4" style={{gridTemplateColumns:"repeat(3,1fr)",}}>
         
         <a href="Poultry">
-            <img src="chefknife.png"></img>
-            <p>Poultry</p>
+            <img src="https://res.cloudinary.com/dm6wodni6/image/upload/v1739975938/poultry_in7ywu.png"></img>
+            
         </a>
         <a href="Wusthof">
-            <img src="santokuknife.png"></img>
-            <p>Wusthof</p>
+            <img src="https://res.cloudinary.com/dm6wodni6/image/upload/v1739976008/wushtof_iac007.png"></img>
+            
         </a>
         <a href="Household">
-            <img src="breadknife.png"></img>
-            <p>Household</p>
+            <img src="https://res.cloudinary.com/dm6wodni6/image/upload/v1739976124/kitchen_xqxkmi.png"></img>
+            
         </a>
         <a href="HerbScissor">
-            <img src="cleverknife.png"></img>
-            <p>HerbScissor</p>
+            <img src="https://res.cloudinary.com/dm6wodni6/image/upload/v1739976241/herb_ck2uno.png"></img>
         </a>
         <a href="SeafoodScissors">
-            <img src="paringknife.png"></img>
-            <p>Seafood</p>
+            <img src="https://res.cloudinary.com/dm6wodni6/image/upload/v1739976331/seafood_miepsj.png"></img>
         </a>
         <a href="Multipurpose">
-            <img src="steakknife.png"></img>
-            <p>Multipurpose</p>
+            <img src="https://res.cloudinary.com/dm6wodni6/image/upload/v1739976385/Group_10_2_kgue44.png"></img>
         </a>
        
         </div>
