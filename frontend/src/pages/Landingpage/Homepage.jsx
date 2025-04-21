b7cb10d655d71e251f4141c249099734bbf022ea
JAYD <167768863+rjayd03@users.noreply.github.com>
2025-04-21 10:42:12 +0800

    Update Homepage.jsx

--- frontend/src/pages/Landingpage/Homepage.jsx
+++ frontend/src/pages/Landingpage/Homepage.jsx
@@ -131,7 +131,7 @@ const Homepage = () => {
             {/* Header section*/}
 
             <div 
-                className="p-3 p-md-5 d-flex justify-content-center align-items-center flex-column-reverse flex-md-row" 
+                className="p-4 p-md-5 d-flex justify-content-center align-items-center flex-column-reverse flex-md-row" 
                 style={{ minHeight: "min(100vh, 800px)", backgroundColor: "#F0F4F8", position: "relative", overflow: "hidden",
                      display: "flex", justifyContent: "center", alignItems: "center",  }}
             >
